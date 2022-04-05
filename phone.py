8aafc0a7b915594809892c09eedae6888a934077
MohdSanusi <102709155+MohdSanusi@users.noreply.github.com>
2022-04-05 15:28:02 +0800

    Update phone.py

--- phone.py
+++ phone.py
@@ -66,7 +66,6 @@ talk_time = st.slider('talk_time', 1, 24,value=10)
 three_g = st.slider('three_g', 0, 1,value=1)
 touch_screen = st.slider('touch_screen', 0, 1,value=1)
 wifi = st.slider('wifi', 0, 1,value=1)
-price_range = st.slider('price_range', 0, 3,value=1)
 
 prediction = classifier.predict([[battery_power, blue, clock_speed, dual_sim, fc, four_g, int_memory, m_dep, mobile_wt, n_cores, pc, px_height, px_width, ram, sc_h, sc_w, talk_time, three_g, touch_screen, wifi]])
 if prediction == 0:
