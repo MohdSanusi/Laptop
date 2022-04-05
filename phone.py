3f279bda94a5257da4e3f996d6856166f08948d2
MohdSanusi <102709155+MohdSanusi@users.noreply.github.com>
2022-04-05 14:31:24 +0800

    Update phone.py

--- phone.py
+++ phone.py
@@ -1,16 +1,15 @@
 import streamlit as st 
 import numpy as np 
 import pandas as pd
+import pickle
 
 #import matplotlib.pyplot as plt
 from matplotlib import pyplot as plt
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 
 from sklearn.neighbors import KNeighborsClassifier
-#from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 
 st.title('Machine Learning - SMARTPHONE PRICE CLASSIFICATION')
@@ -23,7 +22,6 @@ This is a web app demo using python libraries such as Streamlit, Sklearn etc
 
 st.sidebar.write ("For more info, please contact:")
 st.sidebar.write("<a href='https://www.linkedin.com/in/mohd-sanusi-amat-sernor-9bb8b7195/'>Mohd Sanusi </a>", unsafe_allow_html=True)
-
 
 
 data = pd.read_table('phone.csv', index_col = False,  sep = ',', skipinitialspace = True)
@@ -40,70 +38,30 @@ knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(Xtrain, ytrain)
 ypred = knn.predict(Xtest)
 
-#RandomForest = RandomForestClassifier()
-#RandomForest.fit(Xtrain, ytrain)
-#ypred = RandomForest.predict(Xtest)
+import pickle 
+pickle_out = open('knn.pkl','wb')
+pickle.dump(knn, pickle_out)
+pickle_out.close()
 
-D1 = st.slider('battery_power', 500, 4000,value=1000)
-D2 = st.slider('clock_speed', 0.3, 3.5,value=1.0)
-D3 = st.slider('n_cores', 1, 8,value=1)
-D4 = st.slider('ram', 400, 6000,value=1000)
-D5 = st.slider('int_memory', 4, 128,value=8)
+pickle_in = open('knn.pkl','rb')
+classifier = pickle.load(pickle_in)
 
-#st.write(ypred)
+st.header('Phone Specifications')
+battery = st.slider('battery_power', 500, 4000,value=1000)
+clkspd = st.slider('clock_speed', 0.3, 3.5,value=1.0)
+core = st.slider('n_cores', 1, 8,value=1)
+ram = st.slider('ram', 400, 6000,value=1000)
+storage = st.slider('int_memory', 4, 128,value=8)
 
-st.write("## 3: Classification Report")
-
-if len(X)!=0 and len(y)!=0: 
-
-
-  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_data_ratio, random_state=random_state)
-
-  scaler = StandardScaler()
-  X_train_scaled = scaler.fit_transform(X_train)
-  X_test_scaled = scaler.transform(X_test)    
-
-  clf.fit(X_train_scaled, y_train)
-  y_pred = clf.predict(X_test_scaled)
-
-
-  st.write('Classifier:',classifier_name)
-  st.write('Classification report:')
-  report = classification_report(y_test, y_pred,output_dict=True)
-  df = pd.DataFrame(report).transpose()
-  st.write(df)
-
-else: 
-   st.write("<font color='Aquamarine'>Note: No classification report generated.</font>", unsafe_allow_html=True)
-
-st.write("## 4: Principal Component Analysis Plot")
-suitable = 1
-if len(X_names) <2:
-  st.write("<font color='Aquamarine'>Note: No PCA plot as it requires at least two predictors.</font>", unsafe_allow_html=True)
-  suitable = 0
-else:
-    for names in X_names:
-        if names in cat_var:
-           st.write("<font color='Aquamarine'>Note: No PCA plot as it only supports numerical predictors.</font>", unsafe_allow_html=True)
-           suitable = 0
-           break
-
-if suitable == 1:
-   pca = PCA(2)
-   X_projected = pca.fit_transform(X)
-
-   x1 = X_projected[:, 0]
-   x2 = X_projected[:, 1]
-
-   fig = plt.figure()
-   plt.scatter(x1, x2,
-               c=y, alpha=0.8,
-               cmap='viridis')
-
-   plt.xlabel('Principal Component 1')
-   plt.ylabel('Principal Component 2')
-   plt.colorbar()
-   st.pyplot(fig)
+prediction = classifier.predict([[battery, clkspd, core, ram, storage]])
+  if prediction == 0:
+    st.write('This is a Low-Range Phone')
+  elif prediction == 1:
+    st.write('This is a Mid-Range Phone')
+  elif prediction == 2:
+      st.write('This is a High-End Phone')
+  else:
+    st.write('This is a Flagship Phone')
 
 hide_streamlit_style = """
             <style>
