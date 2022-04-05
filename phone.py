f55d9bf228972fcb49a824a6c0c307a28d58dd3d
MohdSanusi <102709155+MohdSanusi@users.noreply.github.com>
2022-04-05 15:57:50 +0800

    Update phone.py

--- phone.py
+++ phone.py
@@ -69,7 +69,7 @@ touch_screen = st.slider('Touch Screen', 0, 1,value=1)
 wifi = st.slider('WiFi', 0, 1,value=1)
 
 #submit = st.button('Predict')
-st.sidebar.write("<font color='Aquamarine'>Prediction:</font>"
+st.sidebar.write("<font color='Aquamarine'>Prediction:</font>")
 #if submit:
 prediction = classifier.predict([[battery_power, blue, clock_speed, dual_sim, fc, four_g, int_memory, m_dep, mobile_wt, n_cores, pc, px_height, px_width, ram, sc_h, sc_w, talk_time, three_g, touch_screen, wifi]])
 if prediction == 0:
