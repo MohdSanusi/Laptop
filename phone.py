9366853b3ad44743ce31b1775de8454b3ffeb4d4
MohdSanusi <102709155+MohdSanusi@users.noreply.github.com>
2022-04-05 15:23:58 +0800

    Update phone.py

--- phone.py
+++ phone.py
@@ -68,7 +68,7 @@ touch_screen = st.slider('touch_screen', 0, 1,value=1)
 wifi = st.slider('wifi', 0, 1,value=1)
 price_range = st.slider('price_range', 0, 3,value=1)
 
-prediction = classifier.predict([battery_power, blue, clock_speed, dual_sim, fc, four_g, int_memory, m_dep,	mobile_wt, n_cores, pc, px_height, px_width, ram, sc_h, sc_w, talk_time, three_g, touch_screen, wifi, price_range])
+prediction = classifier.predict([[battery_power, blue, clock_speed, dual_sim, fc, four_g, int_memory, m_dep,	mobile_wt, n_cores, pc, px_height, px_width, ram, sc_h, sc_w, talk_time, three_g, touch_screen, wifi, price_range]])
 if prediction == 0:
   st.write('This is a Low-Range Phone')
 elif prediction == 1:
